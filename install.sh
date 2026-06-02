3f92e5e1e38cb0532352d322d82cebff154a5e1c
Kelvin Chappell <1722550+kelvin-chappell@users.noreply.github.com>
2026-06-02 16:28:01 +0100

    Update paths

--- install.sh
+++ install.sh
@@ -4,14 +4,15 @@
 mkdir -p "$HOME/.config"
 
 # Symlink Neovim config
-#ln -sfn "$HOME/code/my-dotfiles/.config/nvim" "$HOME/.config/nvim"
+#ln -sn "$HOME/code/my-dotfiles/.config/nvim" "$HOME/.config/nvim"
 
 # Symlink dev container config
-ln -sfn "$HOME/code/my-dotfiles/.config/devenv/devenv.yaml" "$HOME/.config/devenv/devenv.yaml"
+mkdir -p "$HOME/.config/devenv"
+ln -sn "$HOME/code/my-dotfiles/.config/devenv" "$HOME/.config/devenv"
 
 # Symlink shell files
-ln -sf "$HOME/code/my-dotfiles/shell/.zshrc" "$HOME/.zshrc"
-ln -sf "$HOME/code/my-dotfiles/git/.gitconfig" "$HOME/.gitconfig"
-ln -sf "$HOME/code/my-dotfiles/git/.gitignore_global" "$HOME/.gitignore_global"
+ln -s "$HOME/code/my-dotfiles/shell/.zshrc" "$HOME/.zshrc"
+ln -s "$HOME/code/my-dotfiles/git/.gitconfig" "$HOME/.gitconfig"
+ln -s "$HOME/code/my-dotfiles/git/.gitignore_global" "$HOME/.gitignore_global"
 
 echo "Dotfiles symlinked successfully!"
