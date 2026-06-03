fd434c2e02258fd67c2931dbfb843b700bf0469a
Kelvin Chappell <1722550+kelvin-chappell@users.noreply.github.com>
2026-06-03 08:32:42 +0100

    Update paths

--- install.sh
+++ install.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-# Create the XDG config directory if it doesn't exist
+# Create the XDG config structure
 mkdir -p "$HOME/.config"
-
-# Symlink Neovim config
-#ln -sn "$HOME/code/my-dotfiles/.config/nvim" "$HOME/.config/nvim"
-
-# Symlink dev container config
 mkdir -p "$HOME/.config/devenv"
-ln -sfn "$HOME/code/my-dotfiles/.config/devenv" "$HOME/.config/devenv"
 
-# Symlink shell files
-ln -sf "$HOME/code/my-dotfiles/shell/.zshrc" "$HOME/.zshrc"
-ln -sf "$HOME/code/my-dotfiles/git/.gitconfig" "$HOME/.gitconfig"
-ln -sf "$HOME/code/my-dotfiles/git/.gitignore_global" "$HOME/.gitignore_global"
+# Symlink folders
+#ln -sn "$HOME/code/dotfiles/.config/nvim" "$HOME/.config/nvim"
+ln -sn "$HOME/code/dotfiles/.config/devenv" "$HOME/.config/devenv"
+
+# Symlink root-level files
+ln -s "$HOME/code/dotfiles/shell/.zshrc" "$HOME/.zshrc"
+ln -s "$HOME/code/dotfiles/git/.gitconfig" "$HOME/.gitconfig"
+ln -s "$HOME/code/dotfiles/git/.gitignore_global" "$HOME/.gitignore_global"
 
 echo "Dotfiles symlinked successfully!"
